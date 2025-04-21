908eabb9304b60cc6bc1ffe091747754f9946967
parhamArdeshiri <parhammyk@gmail.com>
2025-04-21 20:45:25 +0330

    Delete askeverything directory

--- askeverything/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import mainView, signup, login, logout_view
-
-urlpatterns = [
-    path('loginorsignup/', mainView),
-    path('signup/', signup),
-    path('login/', login),
-    path('logout/', logout_view),
-]
